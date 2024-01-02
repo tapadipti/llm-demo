d331e3cbf19868eaea46821e46cb83b7098c7f91
tapadipti <tapadipti@gmail.com>
2024-01-02 09:23:45 +0000

    Use all questions in qa.py

--- qa.py
+++ qa.py
@@ -21,7 +21,7 @@ with open("faiss_store.pkl", "rb") as f:
     store = pickle.load(f)
 
 df = pd.read_csv("progit_questions.csv")
-sample_questions = df["Q"].to_list()[0:2]
+sample_questions = df["Q"].to_list()
 
 store.index = index
 llm = Ollama(model="llama2")
